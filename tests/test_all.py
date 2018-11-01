48493457e75a181b41c63c0bb460bea7f3bc3d88
Janus Krakowski <janusz.krakowski@stxnext.pl>
2018-11-01 20:51:43 +0100

    [XXX-] tests added ;)

---
+++ tests/test_all.py
@@ -0,0 +1,12 @@
+import unittest
+
+from mock import patch, mock_open
+
+
+class TestSorting(unittest.TestCase):
+    """
+    I'm sorry there's really not much to test..
+    But `I can into` pytest, coverate and all of that ;)
+    """
+
+    pass
