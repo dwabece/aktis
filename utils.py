6a33ea5b4e8e736a5229f8428fd6352ecdea4c07
Janus Krakowski <janusz.krakowski@stxnext.pl>
2018-11-01 21:02:49 +0100

    [XXX-] pydoc added

--- utils.py
+++ utils.py
@@ -3,6 +3,16 @@ import config as config
 
 
 def load_input(fname, input_type):
+    """
+    Loads file with data to process
+
+    Depending on input_type provided will load from different directories.
+    * `mouse` for task with mouse classifying
+    * `median` for task that counts list median
+
+    :param input_file: name of file located inside data directory
+    :return: file contents
+    """
     types = {
         'mouse': config.MOUSE_DIR,
         'median': config.MEDIAN_DIR,
