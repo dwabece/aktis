d36d6fa1c3ca50d7c8ed3819e54d5f6492ca0cfd
Janus Krakowski <janusz.krakowski@stxnext.pl>
2018-11-01 20:38:15 +0100

    [XXX-] init

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+
+BASE_PATH = os.path.dirname(os.path.abspath(__file__))
+DATA_PATH = os.path.join(BASE_PATH, 'data')
+MOUSE_DIR = os.path.join(DATA_PATH, 'mouse-vs-mouse')
+MEDIAN_DIR = os.path.join(DATA_PATH, 'running-median')
